2e65c69276e1a451df53cc30c887ed6fea53f095
Gabriel <gabrielferreirasilva04@outlook.com>
2023-10-08 23:20:29 -0300

    implementação das boas vindas

---
+++ index.js
@@ -0,0 +1,16 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextName();
+
+},5000)
+
+function nextName(){
+    count++;
+    if(count > 4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}
